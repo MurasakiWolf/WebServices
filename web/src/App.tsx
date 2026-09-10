52e5627a50bdee293319ff7c186de1598dab847c
MurasakiWolf <villalobosaraizajorgeluis@gmail.com>
2026-09-09 23:59:58 -0600

    p2 - add types

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,12 +1,15 @@
-import { Activity, useEffect, useState } from "react"
+import { useEffect, useState } from "react"
+
 
 function App() {
-  const [activities, setActivities] = useState([])
+  const [activities, setActivities] = useState<Activity[]>([])
 
   useEffect(() => {
     fetch('https://localhost:5001/api/v1/events')
       .then(response => response.json())
       .then(data => setActivities(data))
+
+    return () => { };
   }, [])
 
   return (
@@ -14,9 +17,7 @@ function App() {
       <h3 className="app" style={{ color: 'red' }}>Web Service</h3>
       <ul>
         {activities.map((activity) => (
-          <li key={activity.id}>
-            {activity.title}
-          </li>
+          <li key={activity.id}> {activity.title} </li>
         ))}
       </ul>
     </div>
