f011f9f5793e194668bd9d80ac91b66ed38f485a
MurasakiWolf <villalobosaraizajorgeluis@gmail.com>
2026-09-23 22:37:26 -0600

    p2 - add create event

--- src/WebService.Api/Controllers/EventsController.cs
+++ src/WebService.Api/Controllers/EventsController.cs
@@ -2,20 +2,32 @@ using Microsoft.AspNetCore.Mvc;
 using WebService.Domain;
 using MediatR;
 using WebService.Application.Events.Queries;
+using WebService.Application.Events.Commands;
 
 namespace WebService.Api.Controllers;
 
 public class EventsController : WebServiceBaseController
 {
     [HttpGet]
+    [ProducesResponseType(typeof(IReadOnlyList<Event>), StatusCodes.Status200OK)]
     public async Task<ActionResult<IReadOnlyList<Event>>> GetEventsAsync()
     {
         return await Mediator.Send(new GetEventList.Query());
     }
 
     [HttpGet("{id}")]
+    [ProducesResponseType(typeof(Event), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(Event), StatusCodes.Status404NotFound)]
     public async Task<ActionResult<Event>> GetEventDetailAsync(string id)
     {
         return await Mediator.Send(new GetEventDetails.Query { Id = id });
+    }
+
+    [HttpPost]
+    [ProducesResponseType(typeof(string), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(string), StatusCodes.Status400BadRequest)]
+    public async Task<ActionResult<string>> CreateEventAsync(Event @event)
+    {
+        return await Mediator.Send(new CreateEvent.Command { Event = @event });
     }
 }
